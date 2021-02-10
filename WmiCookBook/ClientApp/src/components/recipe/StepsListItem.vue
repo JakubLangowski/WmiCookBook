2449e7448a608104238ce767557647eec57fe9f7
Kamil Drzastwa <kamil@codeaddict.io>
2021-02-10 04:00:55 +0100

    FIXING IMAGE SIZING

---
+++ WmiCookBook/ClientApp/src/components/recipe/StepsListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <tr>
+    <td>
+      <CheckboxInput :name="name" :text="name"/>
+    </td>
+    <td>
+      {{ quantity }}
+    </td>
+  </tr>
+</template>
+
+<script>
+import CheckboxInput from "@/components/shared/CheckboxInput";
+
+export default {
+  name: "IngredientsLisItem",
+  components: {CheckboxInput},
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
